b1d53951812b1e93cbdca575222f014c1f65ee7b
Vishalsai Daswani <vishalsai@gmail.com>
2016-01-10 14:44:28 -0800

    routes.js: remove log message

--- routes.js
+++ routes.js
@@ -1,14 +1,12 @@
 FlowRouter.route("/", {
     name: "gameViewer",
     action: function(params) {
-        console.log("Watching the game");
     }
 });
 
 FlowRouter.route("/player1", {
     name: "p1",
     action: function(params) {
-        console.log("Player 1 entered the game");
         Session.set("player", "p1");
     }
 });
@@ -16,7 +14,6 @@ FlowRouter.route("/player1", {
 FlowRouter.route("/player2", {
     name: "p2",
     action: function(params) {
-        console.log("Player 2 entered the game");
         Session.set("player", "p2");
     }
 });
