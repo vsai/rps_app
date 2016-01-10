c408ef6baeeea86cd4278674c41e2b78659c7a33
Vishalsai Daswani <vishalsai@gmail.com>
2016-01-10 15:04:58 -0800

    full refactor: separate files into separate directories
    separate server code from client code.
    separate shared Meteor.methods into lib director
    put collection definition into own collection/ dir
    
    functionaly the same. this change is primarily to separate client code
    from server code and to make future modifications simpler.

---
+++ client/rps_app.js
@@ -0,0 +1,30 @@
+Meteor.subscribe("currentGame");
+Template.body.helpers({
+    games: function() {
+        return Games.find({});
+    }
+});
+Template.main_game.events({
+    "click .rock": function() {
+        Session.set("myMove", "rock");
+        var player = Session.get("player");
+        Meteor.call("updateGame", player, "rock");
+    },
+    "click .paper": function() {
+        Session.set("myMove", "paper");
+        var player = Session.get("player");
+        Meteor.call("updateGame", player, "paper");
+    },
+    "click .scissors": function() {
+        Session.set("myMove", "scissors");
+        var player = Session.get("player");
+        Meteor.call("updateGame", player, "scissors");
+    },
+    "click .new_game": function() {
+        Meteor.call("newGame");
+    }
+});
+Template.main_game.helpers({
+    myMove: function() { return Session.get("myMove");},
+    player: function() { return Session.get("player");}
+});
